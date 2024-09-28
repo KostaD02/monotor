c7ffdcf974e8eaa7214752eae12bc00ba1078495
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-09-28 15:52:44 +0400

    chore: angular library generator boilerplate

---
+++ tools/generators/src/angular-lib/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { angularLibGenerator } from './generator';
+import { AngularLibGeneratorSchema } from './schema';
+
+describe('angular-lib generator', () => {
+  let tree: Tree;
+  const options: AngularLibGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await angularLibGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+});
