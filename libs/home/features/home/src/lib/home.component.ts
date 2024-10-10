d6c034f6afc926110106c76a8572677d104e08bb
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-10-10 21:00:11 +0400

    feat: update home page

--- libs/home/features/home/src/lib/home.component.ts
+++ libs/home/features/home/src/lib/home.component.ts
@@ -1,16 +1,20 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+import { filter, forkJoin, map, switchMap } from 'rxjs';
 
 import { MetricsService } from '@fitmonitor/client-metrics/data-access';
 import { MetricComponent } from '@fitmonitor/metrics/client-metrics/features/ui/metric';
 import { CalendarService } from '@fitmonitor/client-calendar/data-access';
 import { CalendarComponent } from '@fitmonitor/calendar/client-calendar/features/ui/calendar';
 import { FormComponent } from '@fitmonitor/shared/ui/form';
+import { ScheduleService } from '@fitmonitor/schedule/data-access';
+import { ScheduleComponent } from '@fitmonitor/schedule/client-schedule/features/ui/schedule';
+import { AuthService } from '@fitmonitor/data-access';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { ScheduleService } from '@fitmonitor/schedule/data-access';
-import { ScheduleComponent } from '@fitmonitor/schedule/client-schedule/features/ui/schedule';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 @Component({
   selector: 'fitmonitor-home',
@@ -18,6 +22,7 @@ import { ScheduleComponent } from '@fitmonitor/schedule/client-schedule/features
   imports: [
     NzLayoutModule,
     NzTabsModule,
+    NzEmptyModule,
     FormComponent,
     MetricComponent,
     CalendarComponent,
@@ -28,11 +33,27 @@ import { ScheduleComponent } from '@fitmonitor/schedule/client-schedule/features
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  private readonly authService = inject(AuthService);
   private readonly metricsService = inject(MetricsService);
   private readonly scheduleService = inject(ScheduleService);
   private readonly calendarsService = inject(CalendarService);
 
-  readonly metrics = toSignal(this.metricsService.getAllMetrics());
-  readonly calendars = toSignal(this.calendarsService.getAllCalendars());
-  readonly schedules = toSignal(this.scheduleService.getAllSchedules());
+  readonly usersData = toSignal(
+    toObservable(this.authService.user).pipe(
+      filter(Boolean),
+      switchMap(() =>
+        forkJoin([
+          this.metricsService.getAllMetrics(),
+          this.calendarsService.getAllCalendars(),
+          this.scheduleService.getAllSchedules(),
+        ]).pipe(
+          map(([metrics, calendars, schedules]) => ({
+            metrics,
+            calendars,
+            schedules,
+          })),
+        ),
+      ),
+    ),
+  );
 }
