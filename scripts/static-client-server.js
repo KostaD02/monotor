17ff78020b4178c9873fb7e80d75bc70cb90480e
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-03-16 15:06:27 +0400

    feat: change nginx to static server from node.js

---
+++ scripts/static-client-server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const PROTOCOL = process.env.PROTOCOL || 'http';
+const PORT = process.env.PORT || 2202;
+
+const BASE_URL = path.join(__dirname, 'monotor-client-dist');
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(BASE_URL));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(BASE_URL, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `[CLIENT] [LOG] [${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] Server is running on ${PROTOCOL}://YOUR_LOCAL_IP_ADDRESS:${PORT}`,
+  );
+});
